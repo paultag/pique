5adc5a658b8a9a384639dd82f58109a6b16b774e
Paul Tagliamonte <tag@pault.ag>
2012-02-23 22:04:23 -0500

    foo

--- qview/models.py
+++ qview/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class QueueItem(models.Model):
+    bugno    = models.IntegerField(primary_key=True)
     active   = models.BooleanField()
     reporter = models.CharField(max_length=80)
     owner    = models.CharField(max_length=80)
