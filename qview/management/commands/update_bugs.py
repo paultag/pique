24a8d68bc6a2e6bddcd64750cf0cf216098bab03
Paul Tagliamonte <tag@pault.ag>
2012-02-23 22:08:24 -0500

    foo

--- qview/management/commands/update_bugs.py
+++ qview/management/commands/update_bugs.py
@@ -1,9 +1,22 @@
 from django.core.management.base import BaseCommand, CommandError
 from pyqie.qview.models import QueueItem
 
+import SOAPpy
+
+url = 'http://bugs.debian.org/cgi-bin/soap.cgi'
+namespace = 'Debbugs/SOAP'
+server = SOAPpy.SOAPProxy(url, namespace)
+
+package_name = "sponsorship-requests"
+
+def get_all_bugs():
+    return server.get_bugs("package", package_name)
+
 class Command(BaseCommand):
     args = ''
     help = 'Import all the bugs'
 
     def handle(self, *args, **options):
-        self.stdout.write('Successfully closed poll\n')
+        bugs = get_all_bugs()
+        for bug in bugs:
+            self.stdout.write(str(bug) + "\n")
