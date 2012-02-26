c2488b2549db0206dc7c8ea8d4329ed8c92a1e5d
Paul Tagliamonte <tag@pault.ag>
2012-02-25 23:44:06 -0500

    foo

--- qview/urls.py
+++ qview/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('pyqie.qview.views',
-    url(r'^$', 'home', name='home'),
+    url(r'^$',                        'home',   name='home'),
+    url(r'^buginf/(?P<buginf>\d+)/$', 'buginf', name='buginf' ),
 )
--- qview/views.py
+++ qview/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, redirect
 from pyqie.qview.models import QueueItem
 
+from collections import defaultdict
+
 def home(request):
     tickets = QueueItem.objects.filter(
         active=True
     ).order_by('bugno')
+
     return render( request, "qview/home.html", {
         "tickets" : tickets
+    })
+
+def buginf(request, buginf=None):
+    ticket = QueueItem.objects.get(
+        bugno=buginf
+    )
+
+    return render( request, "qview/buginf.html", {
+        "ticket" : ticket
     })
